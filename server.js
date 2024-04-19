c5c872af1326ce25918f16efcb4b167ddc5164b9
fmarivas <felixviage@gmail.com>
2024-04-19 18:29:34 +0200

    log do server

--- server.js
+++ server.js
@@ -23,20 +23,20 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', routes)
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.use('/', routes)
 
-//error
-app.use((err, req, res, next) => {
-    if (err.status === 403) {
-        res.status(403).render('error/error403', {err}); // Assume que você tem um arquivo error403.ejs
-    } else if (err.status === 404) {
-        res.status(404).render('error/error404', {err}); // Pode ser redundante se você já tem um middleware 404
-    } else {
-        console.error(err.stack); // Log do erro no console do servidor
-        res.status(500).render('error/error500', {err }); // Trata todos os outros erros como 500
-    }
-});
+// error
+// app.use((err, req, res, next) => {
+    // if (err.status === 403) {
+        // res.status(403).render('error/error403', {err}); // Assume que você tem um arquivo error403.ejs
+    // } else if (err.status === 404) {
+        // res.status(404).render('error/error404', {err}); // Pode ser redundante se você já tem um middleware 404
+    // } else {
+        // console.error(err.stack); // Log do erro no console do servidor
+        // res.status(500).render('error/error500', {err }); // Trata todos os outros erros como 500
+    // }
+// });
 
 app.listen(port, () =>{
 	console.log(`Server running on port ${port}`)
