856fa718e1028b56184c18185eaef38215ee9560
fmarivas <felixviage@gmail.com>
2024-04-30 09:41:47 +0200

    Verificacao do problemas de login 15

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,  // 30 dias
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+	proxy: true,
   }
 }));
 
