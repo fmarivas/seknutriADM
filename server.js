29b6dd4242fc9c2a85ba3ac34b728f28f875f5fb
fmarivas <felixviage@gmail.com>
2024-05-01 12:15:57 +0200

    Verificacao do problemas de login 17, adicao de passport 21 success 2

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ app.use(session({
   saveUninitialized: false, //antes true
   store: sessionStore, //Aqui
   cookie: {
-	// secure: false,
 	secure: process.env.NODE_ENV === 'production',  // Uso de HTTPS
 	httpOnly: true,
 	sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
